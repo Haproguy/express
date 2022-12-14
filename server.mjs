42041e63c65975ccd7711b3644d2e13ee95020e5
User -2022DRWRV\Administrator <kims2095@gmail.com>
2022-12-14 21:22:20 +0900

    mongodb연동 에러 fix중

--- server.mjs
+++ server.mjs
@@ -1,21 +1,39 @@
 import express from 'express';
 import path from 'path';
+import { MongoClient } from 'mongodb';
+
 const __dirname = path.resolve();
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(8080, function () {
-    console.log("listening on 8080");
+let db;
+
+MongoClient.connect("mongodb+srv://serverHost:qwer1234@clusteryk.q2ypo2r.mongodb.net/ToDoApp?retryWrites=true&w=majority", { useUnifiedTopology: true }, (err, client) => {
+
+    if (err) return console.log(err);
+
+    db = client.db('ToDoApp');
+
+    db.collection('post').insertOne();
+
+    app.listen(8080, () => {
+        console.log('listening on 8080');
+    });
 });
 
-// 누가 /pet으로 방문하면 pet관련 안내문으로 띄워 주자
-app.get("/pet", function (req, res) {
-    res.send("반갑습니다.");
-});
-
-app.get("/beauty", function (req, res) {
-    res.send("뷰티용품 반갑습니다");
-});
-
-app.get('', function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
+
+app.get("/write", (req, res) => {
+    res.sendFile(__dirname + "/write.html");
+})
+
+// 어떤사람이 /add 경로로 post 요청하면 ??를 하세요
+app.post("/add", (req, res) => {
+    res.send("전송완료");
+    console.log(req.body.title);
+    console.log(req.body.date);
+    //db에 저장
+});
+
